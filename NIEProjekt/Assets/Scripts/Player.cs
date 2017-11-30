cd17fccb6e036d44a4f94a5dce23cdedb5a6d8af
Kajetan Kaczmarek <kajkacz@wp.pl>
2017-11-30 14:43:39 +0100

    Added player movement

---
+++ NIEProjekt/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerMovement : MonoBehaviour {
+
+    private float speed = 10f;
+    private Vector3 moveDir = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update () {
+        CharacterController controller = gameObject.GetComponent<CharacterController>();
+
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical");
+
+            moveDir = transform.TransformDirection(moveDir);
+
+
+        }
+	}
+}
